6e846ba6a3102e0be21a180a5c6f846bd6952b0c
Andrey Krivosheev <feuiuxbe@gmail.com>
2021-03-26 04:10:44 +0100

    init: create count-cats.js basic algo

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(arrayCats) {
+  for (let i=0; i < arrayCats.length; i++) {
+    for (let k=0; k < arrayCats[i].length; k++) {
+      var stringToArray = arrayCats[i][k].toString();
+    }
+  }
+  if (stringToArray.search("^^") === -1) return 0;
+  let catsCount = 0;
+  for (let i=0; i < stringToArray.length; i++) {
+    if (stringToArray.search("^^")) {
+      catsCount = catsCount + 1;
+    } else {
+      return catsCount;
+    }
+  }
 };
