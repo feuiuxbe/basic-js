58f52074e3955d7fd7a09ec8992cce4622cbc8ef
Andrey Krivosheev <feuiuxbe@gmail.com>
2021-03-27 18:10:23 +0100

    fix: support float point test
    7/7 tests are passed, hurray!

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,10 +6,10 @@ const HALF_LIFE_PERIOD= 5730;
 module.exports = function dateSample(sampleActivity) {
   if (
       (typeof sampleActivity !== 'string') ||
-      (sampleActivity.match("[^0-9]")) ||
+      (sampleActivity.match("[^0-9*\.?0-9*]")) ||
       (sampleActivity.length === 0)
      ) return false;
-  var sampleActivityToNumber = parseInt(sampleActivity, 10);
+  var sampleActivityToNumber = parseFloat(sampleActivity, 10);
   if (sampleActivityToNumber <= 0 && sampleActivityToNumber > 10) {
     return false;
   }
