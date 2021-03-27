a64c2de7fb019830210fbc61caabdb10c362b278
Andrey Krivosheev <feuiuxbe@gmail.com>
2021-03-27 18:37:27 +0100

    init: add dream-team.js
    added general logic to follow

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(membersArray) {
+  /* общая логика:
+  if not membersArray - false
+  Array должен содержать strings (и только их)
+  Если не стрингс - игнорировать 
+  В каждой стрингс найти первую букву (могут начинаться с пробелов)
+  Объединить первые буквы в строку
+  Поднять все буквы в строке до верхнего регистра
+  Отсортировать все буквы в верхнем регистре от А до Зет
+  Вернуть результат сортировки 
+  */
 };
