d76bfd109a4c2787956e5dc89ca5136a9d0f64d9
Andrey Krivosheev <feuiuxbe@gmail.com>
2021-03-28 17:24:41 +0200

    fix: check if Date is really Date
    9/9 tests passed, hurray!

--- src/what-season.js
+++ src/what-season.js
@@ -3,6 +3,7 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function getSeason(seasonDate) {
  if (seasonDate === null || seasonDate == undefined) return 'Unable to determine the time of year!';
  if (typeof seasonDate !== 'object') {throw new Error('Hello, this is error')};
+ if (Object.prototype.toString.call(seasonDate) !== '[object Date]'){throw new Error('Hello, this is tricky error');}
  if (seasonDate.getMonth() === 11 || seasonDate.getMonth() === 0 || seasonDate.getMonth() === 1) return 'winter';
  if (seasonDate.getMonth() === 2 || seasonDate.getMonth() === 3 || seasonDate.getMonth() === 4) return 'spring';
  if (seasonDate.getMonth() === 5 || seasonDate.getMonth() === 6 || seasonDate.getMonth() === 7) return 'summer';
